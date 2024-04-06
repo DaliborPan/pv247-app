03997796a1c8016e5e629e51be9497010196006a
Dalibor Pantlik <pantlik.dalibor@seznam.cz>
2024-04-07 00:00:04 +0200

    mdx for general info

--- src/app/(with-sidebar)/page.tsx
+++ src/app/(with-sidebar)/page.tsx
@@ -1,8 +1,10 @@
+import Link from 'next/link';
+
 import { Button } from '@/components/base/button';
 import { LecturePreview } from '@/components/lecture-preview';
 import { query } from '@/db/query';
 
-import { GeneralInfo } from '../_components/general-info';
+import GeneralInfo from './_components/general-info.mdx';
 
 const Page = async () => {
 	const availableLectures = await query.getAvailableLectures();
@@ -24,20 +26,24 @@ const Page = async () => {
 							<h3 className="-mt-1 text-xl truncate">{currentLecture.name}</h3>
 						</div>
 
-						<Button
-							variant="primary/inverse"
-							size="sm"
-							iconLeft={{
-								name: 'ArrowRight'
-							}}
-						/>
+						<Link href={`/lectures/${currentLecture.slug}`}>
+							<Button
+								variant="primary/inverse"
+								size="sm"
+								iconLeft={{
+									name: 'ArrowRight'
+								}}
+							/>
+						</Link>
 					</div>
 
 					<LecturePreview>{currentLecture.preview}</LecturePreview>
 				</div>
 			</section>
 
-			<GeneralInfo />
+			<main>
+				<GeneralInfo />
+			</main>
 		</>
 	);
 };
