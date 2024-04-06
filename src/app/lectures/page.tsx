b4ae2d79bdac4ba4e3a47e06a0312c1fec0e2d15
Dalibor Pantlik <pantlik.dalibor@seznam.cz>
2024-04-06 08:56:03 +0200

    lecture card styling

--- src/app/lectures/page.tsx
+++ src/app/lectures/page.tsx
@@ -6,6 +6,13 @@ import { Badge } from '@/components/base/badge/badge';
 import { Icon } from '@/components/base/icon';
 import { LecturePreview } from '@/components/lecture-preview';
 
+const getNumberWithOrdinal = (num: number) => {
+	const s = ['th', 'st', 'nd', 'rd'];
+	const value = num % 100;
+
+	return num + (s[(value - 20) % 10] || s[value] || s[0]);
+};
+
 const formatDate = (date: string) => {
 	const d = new Date(date);
 
@@ -16,8 +23,18 @@ const formatDate = (date: string) => {
 	});
 };
 
-const LectureCard = ({ lecture }: { lecture: Lecture }) => (
-	<div className="p-6 bg-white rounded-lg shadow">
+const LectureCard = ({
+	lecture,
+	index
+}: {
+	lecture: Lecture;
+	index: number;
+}) => (
+	<article className="p-6 bg-white rounded-lg shadow">
+		<span className="flex items-center mb-1 text-xs text-gray-500">
+			from {formatDate(lecture.availableFrom)}
+		</span>
+
 		<h2 className="text-xl font-medium">{lecture.name}</h2>
 
 		<LecturePreview className="mt-3 line-clamp-4">
@@ -29,12 +46,12 @@ const LectureCard = ({ lecture }: { lecture: Lecture }) => (
 				<Button size="sm">Open lecture</Button>
 			</Link>
 
-			<Badge variant="outline">
-				<Icon name="Clock" className="mr-2" />
-				{formatDate(lecture.availableFrom)}
+			<Badge variant="outline" className="text-gray-600">
+				<Icon name="Layers" className="mr-2" />
+				{getNumberWithOrdinal(index + 1)} week
 			</Badge>
 		</div>
-	</div>
+	</article>
 );
 
 const Page = async () => {
@@ -45,8 +62,8 @@ const Page = async () => {
 			<h1 className="mb-6 text-3xl">Lectures</h1>
 
 			<div className="grid grid-cols-2 gap-6">
-				{lectures.map(lecture => (
-					<LectureCard key={lecture.slug} lecture={lecture} />
+				{lectures.map((lecture, index) => (
+					<LectureCard key={lecture.slug} lecture={lecture} index={index} />
 				))}
 			</div>
 		</>
