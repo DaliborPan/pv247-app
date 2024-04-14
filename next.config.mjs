1528cebf19e87b92b895e203bb4d647a7ae44872
Dalibor Pantlik <pantlik.dalibor@seznam.cz>
2024-04-14 07:09:35 +0200

    add pernament redirec

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@ import nextMDX from '@next/mdx';
 import rehypePrettyCode from 'rehype-pretty-code';
 
 const withMDX = nextMDX({
+	redirects: () => [
+		{
+			source: '/lector',
+			destination: '/lector/students',
+			permanent: true
+		}
+	],
+
 	extension: /\.mdx?$/,
 	options: {
 		remarkPlugins: [],
