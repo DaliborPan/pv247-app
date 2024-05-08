8f60268e5fb8c71ba59286f7051ee583d4a4c322
Dalibor Pantlik <pantlik.dalibor@seznam.cz>
2024-05-08 15:28:52 +0200

    use unstable_cache for available lectures query

--- src/db/query/lectures.ts
+++ src/db/query/lectures.ts
@@ -1,15 +1,23 @@
+import { unstable_cache } from 'next/cache';
+
 import { db, type Lecture } from '..';
 
 export const getIsAvailable = (lecture: Lecture) =>
 	new Date(lecture.availableFrom).getTime() < new Date().getTime();
 
-export const getAvailableLectures = async () => {
-	const lectures = await db.query.lectures.findMany({
-		orderBy: (lectures, { asc }) => [asc(lectures.availableFrom)]
-	});
+export const getAvailableLectures = unstable_cache(
+	async () => {
+		const lectures = await db.query.lectures.findMany({
+			orderBy: (lectures, { asc }) => [asc(lectures.availableFrom)]
+		});
 
-	return lectures.filter(getIsAvailable);
-};
+		return lectures.filter(getIsAvailable);
+	},
+	['available-lectures'],
+	{
+		revalidate: 60 * 60 * 24 // one day
+	}
+);
 
 export const getOrderedLectures = async () => {
 	const lectures = await db.query.lectures.findMany({
