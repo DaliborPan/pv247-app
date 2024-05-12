b1745464ee9bf520c95bc108ab32d51701b5b09a
Dalibor Pantlik <pantlik.dalibor@seznam.cz>
2024-05-12 12:40:01 +0200

    fix sidebar homeworks list

--- src/app/(app)/(with-sidebar)/_components/sidebar/homeworks-card.tsx
+++ src/app/(app)/(with-sidebar)/_components/sidebar/homeworks-card.tsx
@@ -15,31 +15,36 @@ export const HomeworksCard = async () => {
 	return (
 		<SidebarCard title="Homeworks" className="hidden lg:block">
 			<div className="flex flex-col gap-y-2">
-				{lectures.slice(0, availableLength + 1).map((lecture, index) => {
-					const isAvailable = index !== availableLength;
+				{lectures
+					.slice(0, availableLength + 1)
+					.filter(lecture => !!lecture.homeworkSlug)
+					.map((lecture, index) => {
+						const isAvailable = index !== availableLength;
 
-					const homework = userHomeworks.find(
-						homework => homework.lectureId === lecture.id
-					);
+						const homework = userHomeworks.find(
+							homework => homework.lectureId === lecture.id
+						);
 
-					return (
-						<SidebarLinkRow
-							key={lecture.slug}
-							href={`/homeworks/${lecture.homeworkSlug}`}
-							isAvailable={isAvailable}
-						>
-							<span className="text-gray-600 grow">{lecture.homeworkName}</span>
-
-							{homework ? (
-								<span className="text-sm font-medium text-primary">
-									{homework.points}/{lecture.homeworkMaxPoints}
+						return (
+							<SidebarLinkRow
+								key={lecture.slug}
+								href={`/homeworks/${lecture.homeworkSlug}`}
+								isAvailable={isAvailable}
+							>
+								<span className="text-gray-600 grow">
+									{lecture.homeworkName}
 								</span>
-							) : (
-								<Icon name={isAvailable ? 'ArrowRight' : 'Lock'} />
-							)}
-						</SidebarLinkRow>
-					);
-				})}
+
+								{homework ? (
+									<span className="text-sm font-medium text-primary">
+										{homework.points}/{lecture.homeworkMaxPoints}
+									</span>
+								) : (
+									<Icon name={isAvailable ? 'ArrowRight' : 'Lock'} />
+								)}
+							</SidebarLinkRow>
+						);
+					})}
 			</div>
 		</SidebarCard>
 	);
