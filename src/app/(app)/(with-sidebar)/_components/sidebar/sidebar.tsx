6bea3e039cda9cb23f83839a6ba52ee577f2fa6f
Dalibor Pantlik <pantlik.dalibor@seznam.cz>
2024-05-12 12:43:42 +0200

    hide some of sidebar cards for lector

--- src/app/(app)/(with-sidebar)/_components/sidebar/sidebar.tsx
+++ src/app/(app)/(with-sidebar)/_components/sidebar/sidebar.tsx
@@ -1,13 +1,21 @@
+import { getSessionUser } from '@/auth/session-user';
+
 import { HomeworksCard } from './homeworks-card';
 import { LecturesCard } from './lectures-card';
 import { OverviewCard } from './overview-card';
 import { ProjectCard } from './project-card';
 
-export const Sidebar = () => (
-	<aside className="lg:fixed lg:top-[100px] lg:h-[calc(100vh-132px)] lg:w-[18.5rem] lg:overflow-y-auto flex flex-col lg:gap-y-8 gap-y-4 pr-3">
-		<OverviewCard />
-		<LecturesCard />
-		<HomeworksCard />
-		<ProjectCard />
-	</aside>
-);
+export const Sidebar = async () => {
+	const user = await getSessionUser();
+
+	return (
+		<aside className="lg:fixed lg:top-[100px] lg:h-[calc(100vh-132px)] lg:w-[18.5rem] lg:overflow-y-auto flex flex-col lg:gap-y-8 gap-y-4 pr-3">
+			{user.role === 'student' && <OverviewCard />}
+
+			<LecturesCard />
+			<HomeworksCard />
+
+			{user.role === 'student' && <ProjectCard />}
+		</aside>
+	);
+};
