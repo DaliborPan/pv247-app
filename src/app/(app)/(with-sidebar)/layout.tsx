e61b19587e172147c46434b323b032e5d41c0b5f
Dalibor Pantlik <pantlik.dalibor@seznam.cz>
2024-11-03 21:52:03 +0100

    update next v14.2.16; add suspense for sidebar

--- src/app/(app)/(with-sidebar)/layout.tsx
+++ src/app/(app)/(with-sidebar)/layout.tsx
@@ -1,10 +1,12 @@
-import { type PropsWithChildren } from 'react';
+import { Suspense, type PropsWithChildren } from 'react';
 
 import { Sidebar } from '@/components/sidebar';
 
 const Layout = ({ children }: PropsWithChildren) => (
   <div className="container">
-    <Sidebar />
+    <Suspense>
+      <Sidebar />
+    </Suspense>
 
     <div className="lg:pl-[20rem] pt-10 lg:pt-0">{children}</div>
   </div>
