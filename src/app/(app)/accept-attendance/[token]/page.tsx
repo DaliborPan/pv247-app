878675ae7f2c8aa25589f2f33394184dd1375332
Dalibor Pantlik <pantlik.dalibor@seznam.cz>
2025-11-25 09:28:34 +0100

    fix accept attendance

--- src/app/(app)/accept-attendance/[token]/page.tsx
+++ src/app/(app)/accept-attendance/[token]/page.tsx
@@ -5,46 +5,51 @@ import { ArrowRight } from 'lucide-react';
 import { Button } from '@/components/base/button';
 import { processAcceptMineAttendanceAction } from '@/modules/student-lecture/action';
 import { lectureLoaders } from '@/modules/lecture/loader';
+import { Suspense } from 'react';
 
-const Page = async (props: { params: Promise<{ token: string }> }) => {
-  const params = await props.params;
+const Page = async ({ params }: PageProps<'/accept-attendance/[token]'>) => {
   const lectures = await lectureLoaders.getOrdered();
 
-  const lecture = lectures.find(
-    lecture => lecture.attendanceToken === params.token
-  );
-
-  if (!lecture) {
-    redirect('/');
-  }
-
-  // special case of calling action in RSC
-  await processAcceptMineAttendanceAction({ lectureId: lecture.id });
-
   return (
-    <>
-      <h1 className="mb-4 text-2xl lg:text-5xl lg:font-light">
-        Attendance accepted
-      </h1>
+    <Suspense>
+      {params.then(async ({ token }) => {
+        const lecture = lectures.find(
+          lecture => lecture.attendanceToken === token
+        );
 
-      <p className="mb-6 text-sm text-text-secondary md:text-base">
-        Your attendance has been accepted for lecture{' '}
-        <span className="font-medium text-text-primary-color">
-          {lecture.name}
-        </span>
-        .
-      </p>
+        if (!lecture) {
+          redirect('/');
+        }
 
-      <Link href={`/lectures/${lecture.slug}`}>
-        <Button
-          variant="outline/primary"
-          iconRight={{ icon: <ArrowRight /> }}
-          className="w-full lg:w-auto"
-        >
-          Start learning
-        </Button>
-      </Link>
-    </>
+        await processAcceptMineAttendanceAction({ lectureId: lecture.id });
+
+        return (
+          <>
+            <h1 className="mb-4 text-2xl lg:text-5xl lg:font-light">
+              Attendance accepted
+            </h1>
+
+            <p className="mb-6 text-sm text-text-secondary md:text-base">
+              Your attendance has been accepted for lecture{' '}
+              <span className="font-medium text-text-primary-color">
+                {lecture.name}
+              </span>
+              .
+            </p>
+
+            <Link href={`/lectures/${lecture.slug}`}>
+              <Button
+                variant="outline/primary"
+                iconRight={{ icon: <ArrowRight /> }}
+                className="w-full lg:w-auto"
+              >
+                Start learning
+              </Button>
+            </Link>
+          </>
+        );
+      })}
+    </Suspense>
   );
 };
 
