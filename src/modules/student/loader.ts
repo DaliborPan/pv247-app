7cd7275a2a4b57ec782dad753dbfc08842caf4a2
Dalibor Pantlik <pantlik.dalibor@seznam.cz>
2025-11-25 09:21:14 +0100

    move overview query to loader

--- src/modules/student/loader.ts
+++ src/modules/student/loader.ts
@@ -2,8 +2,12 @@ import { cache } from 'react';
 
 import { getSessionUser } from '@/modules/session-user';
 import { type UserType } from '@/modules/user/schema';
+import { homeworkQueries } from '@/modules/homework/server';
+import { lectureQueries } from '@/modules/lecture/server';
+import { projectQueries } from '@/modules/project/server';
+import { getStudentLecturesQuery } from '@/modules/student-lecture/server';
 
-import { getProjectFormStudentComboboxQuery, studentQueries } from './server';
+import { getProjectFormStudentComboboxQuery } from './server';
 
 /**
  * Loads students, that are not assigned to a project and are not the current user.
@@ -21,13 +25,39 @@ export const getProjectFormStudentComboboxLoader = async (
 const getOverview = async (user: UserType) => {
   const sessionUser = await getSessionUser();
 
-  return studentQueries.getOverview(sessionUser, user);
+  const [homework, lectures, attendances] = await Promise.all([
+    homeworkQueries.getMany(sessionUser, { userId: user.id }),
+    lectureQueries.getOrdered(),
+    getStudentLecturesQuery(sessionUser, { userId: user.id })
+  ]);
+
+  // Získání projektu pokud existuje
+  const project = user.projectId
+    ? await projectQueries.get(sessionUser, user.projectId)
+    : undefined;
+
+  // Agregace dat
+  const awardedHomeworkCount = homework.length;
+  const homeworkTotalPoints = homework.reduce(
+    (acc, h) => acc + (h?.points ?? 0),
+    0
+  );
+
+  return {
+    lecturesCount: lectures.length,
+    awardedHomeworkCount,
+    homework,
+    homeworkTotalPoints,
+    project,
+    totalPoints: homeworkTotalPoints + (project?.points ?? 0),
+    attendances
+  };
 };
 
 const getMineOverview = cache(async () => {
   const sessionUser = await getSessionUser();
 
-  return studentQueries.getOverview(sessionUser, sessionUser);
+  return getOverview(sessionUser);
 });
 
 export const studentLoaders = {
--- src/modules/student/server/query.ts
+++ src/modules/student/server/query.ts
@@ -1,57 +1,12 @@
 import { type SessionUserType } from '@/modules/session-user/types';
 
-import { lectureRepository } from '@/modules/lecture/server/repository';
-import { projectRepository } from '@/modules/project/server';
-import { studentLectureRepository } from '@/modules/student-lecture/server';
-import { type UserType } from '@/modules/user/schema';
-
 import { getProjectFormStudents } from './repository';
-import { homeworkRepository } from '@/modules/homework/server';
 
 export const getProjectFormStudentComboboxQuery = async (
   sessionUser: SessionUserType,
   projectId: string | undefined
 ) => getProjectFormStudents(sessionUser, projectId);
 
-const getOverview = async (sessionUser: SessionUserType, user: UserType) => {
-  if (sessionUser.role !== 'lector' && sessionUser.id !== user.id) {
-    throw new Error(
-      `User ${sessionUser.id} is not allowed to view user ${user.id}`
-    );
-  }
-
-  const homework = await homeworkRepository.getMany({ userId: user.id });
-  const awardedHomeworkCount = homework.length;
-  const homeworkTotalPoints = homework.reduce(
-    (acc, h) => acc + (h?.points ?? 0),
-    0
-  );
-
-  const lectures = await lectureRepository.getOrdered();
-
-  const project = !user.projectId
-    ? undefined
-    : (await projectRepository.getMany()).find(
-        project => project.id === user.projectId
-      );
-
-  const attendances = await studentLectureRepository.getMany(user.id);
-
-  return {
-    lecturesCount: lectures.length,
-
-    awardedHomeworkCount,
-    homework,
-    homeworkTotalPoints,
-
-    project,
-
-    totalPoints: homeworkTotalPoints + (project?.points ?? 0),
-    attendances
-  };
-};
-
 export const studentQueries = {
-  getProjectFormStudentCombobox: getProjectFormStudentComboboxQuery,
-  getOverview
+  getProjectFormStudentCombobox: getProjectFormStudentComboboxQuery
 };
