5e77337b67aa74cfc7d9ec00f5e4a6aaf9cf3b19
Dalibor Pantlik <pantlik.dalibor@seznam.cz>
2025-11-26 08:14:28 +0100

    remove ssg

--- src/app/(app)/(auth)/(with-sidebar)/layout.tsx
+++ src/app/(app)/(auth)/(with-sidebar)/layout.tsx
@@ -3,11 +3,11 @@ import { type PropsWithChildren } from 'react';
 import { Sidebar } from '@/components/sidebar';
 
 const Layout = ({ children }: PropsWithChildren) => (
-  <div className="container">
+  <>
     <Sidebar />
 
     <div className="pt-4 lg:pl-[20rem] lg:pt-0">{children}</div>
-  </div>
+  </>
 );
 
 export default Layout;
---
+++ src/app/(app)/(auth)/layout.tsx
@@ -0,0 +1,29 @@
+import { Suspense } from 'react';
+
+import { getSession } from '@/modules/session-user';
+import { redirect } from 'next/navigation';
+import { OnboardingPage } from '@/modules/student/components/onboarding-page';
+
+const Layout = ({ children }: LayoutProps<'/'>) => (
+  /*
+   * We are checking presence of a session cookie in proxy already.
+   * However, it's possible, that the session cookie is expired,
+   * so we better check that also here.
+   *
+   * At the same time, we don't block rendering of static content,
+   * hence the fallback is the children.
+   */
+  <Suspense fallback={children}>
+    {getSession().then(sessionUser => {
+      if (!sessionUser) {
+        redirect('/login');
+      }
+
+      const showOnboarding = !sessionUser.github;
+
+      return showOnboarding ? <OnboardingPage /> : children;
+    })}
+  </Suspense>
+);
+
+export default Layout;
