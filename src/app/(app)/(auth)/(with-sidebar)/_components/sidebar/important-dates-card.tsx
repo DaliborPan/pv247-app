c1f7cbcace7e68948e6eec7f7a34a1f45253a857
DaliborPan <72815195+DaliborPan@users.noreply.github.com>
2026-09-13 13:26:17 +0200

    2026 fall prep (#39)
    * update seed, fix konzole error hydration
    
    * update auth

--- src/app/(app)/(auth)/(with-sidebar)/_components/sidebar/important-dates-card.tsx
+++ src/app/(app)/(auth)/(with-sidebar)/_components/sidebar/important-dates-card.tsx
@@ -5,10 +5,10 @@ import { SidebarCardRow } from './sidebar-card-row';
 export const ProjectTimelineCard = () => (
   <SidebarCard title="Project timeline">
     <div className="flex flex-col gap-y-1">
-      <SidebarCardRow title="Start thinking">23. 3.</SidebarCardRow>
-      <SidebarCardRow title="Submit project spec.">6. 4.</SidebarCardRow>
-      <SidebarCardRow title="Progress review">21/22. 4.</SidebarCardRow>
-      <SidebarCardRow title="Presentation">12/13. 5.</SidebarCardRow>
+      <SidebarCardRow title="Start thinking">19. 10.</SidebarCardRow>
+      <SidebarCardRow title="Submit project spec.">2. 11.</SidebarCardRow>
+      <SidebarCardRow title="Progress review">17/18. 11.</SidebarCardRow>
+      <SidebarCardRow title="Presentation">8/9. 12.</SidebarCardRow>
     </div>
   </SidebarCard>
 );
